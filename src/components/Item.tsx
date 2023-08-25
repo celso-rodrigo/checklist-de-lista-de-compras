dbebc7cb6058182b25bdc2625e1c00478d50fdcd
Celso Rodrigo <celsorodrigoma@gmail.com>
2023-08-25 11:27:37 -0300

    feat: Added "Tag" component inside "Item" component

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,22 +1,30 @@
 import menuIcon from "../assets/menuIcon.svg";
+import ITag from "../interfaces/ITag";
+import Tag from "./Tag";
 
 interface IProps {
   itemBought: boolean;
   itemName: string;
   itemQuantity: number;
   itemQuantityUnity: string;
+  tag: ITag;
 }
 
-function Item({itemBought, itemName, itemQuantity, itemQuantityUnity}: IProps) {
+function Item({itemBought, itemName, itemQuantity, itemQuantityUnity, tag}: IProps) {
   return (
     <div>
       <input
         type="checkbox"
         checked={itemBought}
+        onChange={() => console.log("WIP")}
       />
       <p>{itemName}</p>
       <p>{`${itemQuantity} ${itemQuantityUnity}`}</p>
-      {/* TAG */}
+      <Tag
+        icon={tag.icon}
+        category={tag.category}
+        style="tag"
+      />
       <button
         type="button"
       >
