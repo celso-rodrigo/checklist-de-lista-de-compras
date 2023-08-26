c40681ec34c1becdb017d3537aeb768b0b0407fd
Celso Rodrigo <celsorodrigoma@gmail.com>
2023-08-26 16:00:54 -0300

    feat: Added collor pallet to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      'pink': '#DB5BBF',
+      'orange': '#E07B67',
+      'yellow': '#BB9F3A',
+      'green': '#8CAD51',
+      'blue': '#7B94CB',
+      'pink-dark': '#251622',
+      'orange-dark': '#261A17',
+      'yellow-dark': '#211E12',
+      'green-dark': '#1C2015',
+      'blue-dark': '#1A1D23'
+    },
     extend: {},
   },
   plugins: [],
