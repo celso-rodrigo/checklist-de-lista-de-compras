5dfc884b155c45005bf7a620bdbc696d13b85d18
Celso Rodrigo <celsorodrigoma@gmail.com>
2023-08-28 10:41:32 -0300

    feat: Added new colors to the color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     colors: {
+      'purple-light': '#A881E6',
+      'purple': '#7450AC',
+      'purple-dark': '#523480',
+
+      'gray-100': '#FBF9FE',
+      'gray-200': '#AFABB6',
+      'gray-300': '#252529',
+      'gray-400': '#17171A',
+      'gray-500': '#111112',
+      'gray-600': '#0C0C0D',
+
+      'sucess-light': '#4E995E',
+      'sucess': '#2F723D',
+
       'pink': '#DB5BBF',
       'orange': '#E07B67',
       'yellow': '#BB9F3A',
