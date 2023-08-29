fef5172a5396e90681afd11f4e7154f07dca9c65
Celso Rodrigo <celsorodrigoma@gmail.com>
2023-08-29 13:07:29 -0300

    fix: Fixed brackground size and z index order problems

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import Header from './components/Header'
 import Item from './components/Item'
 import CategoryEnum from './enums/CategoryEnum'
 
 function App() {
   return (
-    <div className='font-inter bg-gray-600 min-h-screen'>
-      <Header />
-      <h1 className='text-gray-100 text-2xl font-bold leading-normal tracking-wide'>
+    <div className='font-inter min-h-screen bg-gray-600 bg-header bg-no-repeat bg-contain'>
+      <h1 className='text-gray-100 text-2xl font-bold leading-normal tracking-wide z-10'>
         Lista de Compras
       </h1>
       <Item
